fdcabe1e302ad6be6627a54e17739eb8e1241063
am2011 <adel.meme.philo@gmail.com>
2022-09-22 16:53:32 -0700

    fix bug

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,10 +3,12 @@ if __name__ == "__main__":
     import sys
     argc = len(sys.argv) - 1
     if (argc == 0):
-        print("{:d}. arguments.".format(argc))
+        print("{:d} arguments.".format(argc))
+    elif (argc == 1):
+        print("{:d} argument:".format(argc))
     else:
         print("{:d} arguments:".format(argc))
-        for str in range(len(sys.argv)):
-            if (str == 0):
+        for indx in range(len(sys.argv)):
+            if (indx == 0):
                 continue
-            print("{:d}: {:s}".format(str, sys.argv[str]))
+            print("{:d}: {:s}".format(indx, sys.argv[indx]))
