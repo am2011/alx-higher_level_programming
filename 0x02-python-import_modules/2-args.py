1aa575f2868894868bafb3d0e885a71bad869a14
am2011 <adel.meme.philo@gmail.com>
2022-09-22 16:35:17 -0700

    fix bug for argument 1

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,9 +3,7 @@ if __name__ == "__main__":
     import sys
     argc = len(sys.argv) - 1
     if (argc == 0):
-        print("{:d} arguments.".format(argc))
-    elif (argc == 1):
-        print("{:d} argument:".format(argc))
+        print("{:d}. arguments.".format(argc))
     else:
         print("{:d} arguments:".format(argc))
         for str in range(len(sys.argv)):
