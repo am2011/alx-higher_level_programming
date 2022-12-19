e8f51bad2e0c09db641e13baf2d2a4e10ff960e3
am2011 <adel.meme.philo@gmail.com>
2022-12-19 22:04:12 +0000

    added 11-second_biggest.js

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -3,7 +3,7 @@
 const args = process.argv.slice(2);
 const argsInt = args.map(s => parseInt(s));
 if (args.length === 0 || args.length === 1) {
-  console.log('0');
+  console.log(0);
 } else {
   console.log(argsInt.sort().reverse()[1]);
 }
