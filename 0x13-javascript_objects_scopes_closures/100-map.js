2cfb9d94b803dfec8f32a4872f608fe38dcf5ebc
am2011 <adel.meme.philo@gmail.com>
2022-12-20 22:10:27 +0000

    added 100-map.js

---
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+// a script that imports an array and computes a new array.
+
+const arr = require('./100-data').list;
+
+
+
+console.log(arr);
+
+console.log(arr.map((x, idx) => x * idx));
