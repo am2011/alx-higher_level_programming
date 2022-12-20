91b822cac702a88382953e4433d442bea0a936d4
adel <78702097+am2011@users.noreply.github.com>
2022-12-20 08:21:38 +0200

    Update 11-second_biggest.js

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -5,5 +5,5 @@ const argsInt = args.map(s => parseInt(s));
 if (args.length === 0 || args.length === 1) {
   console.log(0);
 } else {
-  console.log(argsInt.sort().reverse()[1]);
+  console.log(argsInt.sort(function(a, b){return (a -b)}).reverse()[1]);
 }
