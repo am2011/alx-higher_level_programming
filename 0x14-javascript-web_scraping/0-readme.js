53c3824bb6d6cc810414f3612638f860731d326b
am2011 <adel.meme.philo@gmail.com>
2023-02-09 22:43:25 +0000

    fix

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -8,8 +8,8 @@ const fs = require('fs');
 
 fs.readFile(arg[2], 'utf8', function (err, data) {
 if (err) {
-console.log(err);
+  console.log(err);
 } else {
-process.stdout.write(data);
+  process.stdout.write(data);
 }
 });
